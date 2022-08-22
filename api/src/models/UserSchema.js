487e1165a7c76e0be292dc7b5dd92c6aae78f9fe
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-08-22 22:36:20 +1000

    added the schema with valid datatypes

--- api/src/models/UserSchema.js
+++ api/src/models/UserSchema.js
@@ -1,1 +1,26 @@
 import mongoose from "mongoose";
+
+const userName = new mongoose.Schema({
+  status: {
+    type: String,
+    default: "",
+  },
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: 1,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});

768a8513d4a526a01a229319f5aa0aeaacbd8420
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-08-28 03:42:08 +1000

    login route and get user in the  model

--- api/src/models/UserModel.js
+++ api/src/models/UserModel.js
@@ -4,3 +4,7 @@ import UserSchema from "./UserSchema.js";
 export const insertUser = (obj) => {
   return UserSchema(obj).save();
 };
+
+export const getUser = (filter) => {
+  return UserSchema.findOne(filter);
+};
--- api/src/routers/userRouter.js
+++ api/src/routers/userRouter.js
@@ -1,23 +1,36 @@
 import express from "express";
-import { insertUser } from "../models/userModel.js";
+import { getUser, insertUser } from "../models/userModel.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   try {
-    console.log(req.body);
-
     const result = await insertUser(req.body);
     console.log(result);
     res.json({
       status: "success",
-      message: "please check your email",
+      message: "signed , please check your email",
     });
   } catch (error) {
     if (error.message.includes("E11000 duplicate key error collection")) {
       error.status = 200;
       error.message = "Email already in use, Sorry";
     }
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    console.log("clicked");
+    const { email, password } = req.body;
+
+    const user = await getUser({ email });
+    res.json({
+      status: "success",
+      message: "get router",
+    });
+  } catch (error) {
     next(error);
   }
 });

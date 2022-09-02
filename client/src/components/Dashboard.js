f66548ee5d32282464d7b3e5cafdd5731969e1ca
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-09-02 12:01:39 +1000

    added the transaction form in the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Button, Form, Row } from "react-bootstrap";
+import { MainLayout } from "./layout/MainLayout";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <MainLayout>
+      <Row>{/* <h3 className="mt-5">Dashboard</h3> */}</Row>
+
+      {/* form section */}
+      <h3 className="mt-4 text-center">Dashboard</h3>
+
+      {/* table section */}
+    </MainLayout>
+  );
 };
--- client/src/components/layout/MainLayout.js
+++ client/src/components/layout/MainLayout.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import { Header } from "./Header";
 
-export const MainLayout = () => {
-  return <Header />;
+export const MainLayout = ({ children }) => {
+  return (
+    <div className="">
+      <Header />
+      <Container>{children}</Container>
+    </div>
+  );
 };

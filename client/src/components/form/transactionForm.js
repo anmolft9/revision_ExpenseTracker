d49293642b741cf4f2ec116057d5369614577fab
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-09-03 23:44:49 +1000

    added the new local state to store the form data and send it to db using axios

--- client/src/components/form/transactionForm.js
+++ client/src/components/form/transactionForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Col,
@@ -7,24 +7,58 @@ import {
   Form,
   Row,
 } from "react-bootstrap";
+import { postNewTransaction } from "../../helpers/axiosHelper";
 
 export const TransactionForm = () => {
+  const [form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    // console.log(name, value);
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(form);
+    const result = await postNewTransaction;
+  };
+
   return (
     <div className="mt-5">
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Row className="g-2">
           <Col md="2">
-            <Form.Select defaultValue="">
+            <Form.Select
+              defaultValue=""
+              name="type"
+              onChange={handleOnChange}
+              required
+            >
               <option value="">Choose...</option>
               <option value="Income">Income</option>
               <option value="Expense">Expense</option>
             </Form.Select>
           </Col>
           <Col md="5">
-            <Form.Control name="title" placeholder="Title" />
+            <Form.Control
+              name="title"
+              placeholder="Title"
+              onChange={handleOnChange}
+              required
+            />
           </Col>
           <Col md="2">
-            <Form.Control type="number" name="amount" placeholder="Amount" />
+            <Form.Control
+              type="number"
+              name="amount"
+              placeholder="Amount"
+              onChange={handleOnChange}
+              required
+            />
           </Col>
           <Col md="2">
             <Button type="submit">Add Transaction</Button>
--- client/src/helpers/axiosHelper.js
+++ client/src/helpers/axiosHelper.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const rootUrl = "http://localhost:8000";
 const userEndPoint = rootUrl + "/api/v1/user";
+const transactionEndPoint = rootUrl + "/api/v1/transaction";
 
+//////user api
 export const postNewUser = async (obj) => {
   try {
     const response = await axios.post(userEndPoint, obj);
@@ -19,6 +21,21 @@ export const loginUser = async (obj) => {
   try {
     const response = await axios.post(userEndPoint + "/login", obj);
     console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
+/////transction api
+
+export const postNewTransaction = async (obj) => {
+  try {
+    const response = await axios.post(transactionEndPoint, obj);
+    console.log(response);
     return response.data;
   } catch (error) {
     return {
